0ea3ef0baade3cff7dc3956a49e19ca93a0db242
and028 <91878390+and028@users.noreply.github.com>
2021-11-29 13:28:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
 
+bool primo (int a, int b) {
+
+
+    int modulo=a%b;
+
+
+    if (modulo!=0)  {
+
+        primo(a,b+1);
+
+        return true;
+
+    } else {
+        return false;
+
+    }
+
+
+
+}
