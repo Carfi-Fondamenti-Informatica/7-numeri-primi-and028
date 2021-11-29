2da5add50f9ce014d995b3a64ff09f05d2262b18
and028 <91878390+and028@users.noreply.github.com>
2021-11-29 13:29:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+int main() {
 
-int main(){
-  return 0;
+    int a=0, b=2;
+
+   std::cin>>a;
+
+
+    if (primo(a,b)){
+        std::cout<<"numero primo"<<std::endl;
+    } else {
+        std::cout<<"numero non primo"<<std::endl;
+    }
+
+    return 0;
 }
+
