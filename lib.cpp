2f6e7f3051a8c255cfde6dfe9dfc8656f22b60a9
and028 <91878390+and028@users.noreply.github.com>
2021-11-30 18:50:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,5 @@
 #include "lib.h"
-bool primo (int a, int b) {
+int primo (int a, int b) {
 
     if (a==1) {
         return 0;
@@ -8,3 +8,6 @@ bool primo (int a, int b) {
     return a%b==0 ? a==b : primo(a,b+1);
 
 }
+
+
+
