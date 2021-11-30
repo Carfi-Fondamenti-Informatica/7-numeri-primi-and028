7535af455212ba3dd67b78bdfead2373181dd1ac
and028 <91878390+and028@users.noreply.github.com>
2021-11-30 13:31:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,39 +1,41 @@
 #include "lib.h"
 
-bool primo (int a, int b) {
+int primo (int a, int b) {
 
 
     if (a==0) {
-        return false;
+        return 0;
     }
 
 
 
 
     if (a==1) {
-        return false;
+        return 0;
     }
 
 
 
 
     if (a==2) {
-        return true;
+        return 1;
     }
 
 
     if ((a%b==0) && (a%1==0) && (a==b))  {
 
 
-        return true;
+        return 1;
 
     } else if (a%b==0) {
 
-        return false;
+        return 0;
 
     } else {
 
         primo(a,b+1);
+
+        return primo(a,b+1);
 
 
 
@@ -43,4 +45,3 @@ bool primo (int a, int b) {
 
 
 }
-
