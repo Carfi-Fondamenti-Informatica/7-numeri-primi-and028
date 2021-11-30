9a634758792bc5ae9652d840a3b22669407a09ea
and028 <91878390+and028@users.noreply.github.com>
2021-11-30 10:23:58 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,5 @@
-
-#ifndef COMPITO_7_1_LIB_H
-#define COMPITO_7_1_LIB_H
-bool primo (int,int);
-#endif //COMPITO_7_1_LIB_H
+#ifndef COMPITO_7_2_LIB_H
+#define COMPITO_7_2_LIB_H
+int primo (int,int);
+#endif //COMPITO_7_2_LIB_H
 
