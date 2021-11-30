b59248fd15e2c34227410be45cababb67a7ca4b7
and028 <91878390+and028@users.noreply.github.com>
2021-11-30 18:44:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,62 +1,10 @@
 #include "lib.h"
-
-int primo (int a, int b) {
-
-
-    if (a==0) {
-        return 0;
-    }
-
-
-
+bool primo (int a, int b) {
 
     if (a==1) {
         return 0;
     }
 
-
-
-
-    if (a==2) {
-        return 1;
-    }
-
-
-
-    if (a%2==0) {
-    return 0;
-}
-
-
-    if ((a%b==0) && (a%1==0) && (a==b))  {
-
-
-        return 1;
-
-    } else if (a%b==0) {
-
-        return 0;
-
-    } else {
-
-        int contatore=0;
-        contatore++;
-
-        primo(a,b+2);
-
-
-
-        if ((a%(b+contatore+1)==0) && (a!=(b+contatore+1))) {
-            return 0;
-        } else {
-
-            return 1;
-        }
-
-
-    }
-
-
+    return a%b==0 ? a==b : primo(a,b+1);
 
 }
-
