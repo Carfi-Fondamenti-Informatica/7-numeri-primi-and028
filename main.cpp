8a9b20a04690ca62f504d8d2047cd9fc54deeeeb
and028 <91878390+and028@users.noreply.github.com>
2021-12-01 10:49:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ int main() {
     int a=0, b=2;
 
     std::cin>>a;
-    
     if (primo(a,b)) {
-        std::cout<<"primo";
+        std::cout<<"numero primo";
     } else {
-        std::cout<<"non primo";
+        std::cout<<"numero non primo";
     }
 
 
